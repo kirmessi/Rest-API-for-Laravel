641cdbec61c5c82d44f876a0b1ec418d03aba61c
kirmessi <khitsenko@bk.ru>
2018-07-28 13:21:49 +0300

    Product Update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -83,7 +83,13 @@ public function __construct()
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $request['detail'] = $request->description;
+        unset($request['description']);
+        $product->update($request->all());
+        return response([
+
+            'data' => new ProductResource($product)
+            ],Response::HTTP_CREATED);
     }
 
     /**
--- app/Model/Product.php
+++ app/Model/Product.php
@@ -6,7 +6,15 @@ use App\Model\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
+
 {
+	protected $fillable =[
+
+
+	'name', 'detail', 'stock', 'price', 'discount'
+
+
+	];
    public function reviews()
    {
    	return $this->hasMany(Review::class);
